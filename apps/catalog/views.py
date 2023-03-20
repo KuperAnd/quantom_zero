27ee5073bdd7cd57f4b999307adb70074914aa4b
KuperAnd <ghostofnetwork.null@gmail.com>
2023-03-20 19:46:04 +0300

    Product_mini_category

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -25,3 +25,9 @@ class ProductByCategoryView(generic.ListView):
         context['category'] = self.category
         context['categories'] = self.categories
         return context
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
+
